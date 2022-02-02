6be9dce6ec369a6e73509e310b72684377892ce7
alumnoeficiente <zufri@hotmail.es>
2022-02-02 12:55:43 +0100

    Esto es la Génesis del Proyecto

---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Proyecto_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine ("Escriba algo");
+            string cadena1 = Console.ReadLine();
+            Console.Beep();
+            Console.WriteLine ("Escriba otra cosa");
+            string cadena2 = Console.ReadLine();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            bool Resultado = cadena1.Equals(cadena2); 
+            Console.WriteLine (Resultado);
+            Console.ReadLine();
+            
+          
+          
+
+    
+
+        }
+    
+    } 
+}
